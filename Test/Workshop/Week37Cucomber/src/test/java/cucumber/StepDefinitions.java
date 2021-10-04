90c84640ce3a23c9acb439285139c31f8e6e1fcc
Emil <emilskovbo18@gmail.com>
2021-10-04 09:52:36 +0200

    pulled SI miniporject1 into repo

--- Test/Workshop/Week37Cucomber/src/test/java/cucumber/StepDefinitions.java
+++ Test/Workshop/Week37Cucomber/src/test/java/cucumber/StepDefinitions.java
@@ -5,6 +5,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
+
+
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
