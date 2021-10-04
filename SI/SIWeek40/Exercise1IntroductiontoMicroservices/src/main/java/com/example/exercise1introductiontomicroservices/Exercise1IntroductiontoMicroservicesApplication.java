76bff506647a4e69617fe8e29ea4e7e86558ef8c
Emil <emilskovbo18@gmail.com>
2021-10-04 10:50:40 +0200

    SI week40 exercise1 project created

---
+++ SI/SIWeek40/Exercise1IntroductiontoMicroservices/src/main/java/com/example/exercise1introductiontomicroservices/Exercise1IntroductiontoMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise1introductiontomicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1IntroductiontoMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise1IntroductiontoMicroservicesApplication.class, args);
+    }
+
+}
---
+++ SI/SIWeek40/Exercise1IntroductiontoMicroservices/src/test/java/com/example/exercise1introductiontomicroservices/Exercise1IntroductiontoMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise1introductiontomicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1IntroductiontoMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
